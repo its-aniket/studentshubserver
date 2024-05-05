4ea964a89f7cc9a71441f9b248868434b111928d
its-aniket <aniketjadhav770698@gmail.com>
2024-05-05 17:39:47 +0530

    pushing from localmachine

--- index.js
+++ index.js
@@ -86,8 +86,7 @@ app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
-mongoose
-  .connect(process.env.MONGO_URL, {
+mongoose.connect(process.env.MONGO_URL, {
     
   })
   .then(() => {
